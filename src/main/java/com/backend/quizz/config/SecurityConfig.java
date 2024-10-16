f201303c8acc451d9f4b2f94d784ae272d008c04
shaktiDD <shaktidhardubey7@gmail.com>
2024-10-16 17:08:39 +0530

    connected to frontend

--- src/main/java/com/backend/quizz/config/SecurityConfig.java
+++ src/main/java/com/backend/quizz/config/SecurityConfig.java
@@ -1,9 +1,8 @@
 package com.backend.quizz.config;
 
-import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,31 +12,25 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource(){
-        CorsConfiguration configuration =new CorsConfiguration();
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**",configuration);
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request->request.anyRequest().permitAll())
-                .httpBasic(httpBasic->httpBasic.disable())
-                .cors(c -> c.disable());
+        http.csrf(customizer -> customizer.disable()).authorizeHttpRequests(request -> request.anyRequest().permitAll()).httpBasic(httpBasic -> httpBasic.disable()).cors(c -> c.disable());
         return http.build();
     }
 
